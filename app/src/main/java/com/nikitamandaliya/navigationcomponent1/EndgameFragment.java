03a88a5bcf412aac51693e7d6a1242720b52486d
Nikita Mandaliya <nikitamandaliya55@gmail.com>
2021-06-02 21:11:29 +0530

    SECOND commit - Nikita

---
+++ app/src/main/java/com/nikitamandaliya/navigationcomponent1/EndgameFragment.java
@@ -0,0 +1,41 @@
+package com.nikitamandaliya.navigationcomponent1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class EndgameFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_endgame, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+
+        Button button = view.findViewById(R.id.restartGameButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_endgameFragment_to_startFragment);
+            }
+        });
+    }
+}
--- app/src/main/java/com/nikitamandaliya/navigationcomponent1/MainActivity.java
+++ app/src/main/java/com/nikitamandaliya/navigationcomponent1/MainActivity.java
@@ -3,7 +3,8 @@ package com.nikitamandaliya.navigationcomponent1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+// youtube - https://www.youtube.com/watch?v=akuONx5RTy8&list=RDCMUCr0y1P0-zH2o3cFJyBSfAKg&start_radio=1&t=3s
+// website - https://www.journaldev.com/22299/android-jetpack-navigation-architecture
 public class MainActivity extends AppCompatActivity {
 
     @Override
